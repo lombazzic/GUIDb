280de1801a97214713b3d8351debb8d7242dafb5
Samuele Lombardi <116791046+lombazzic@users.noreply.github.com>
2023-05-16 15:56:49 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace lombardi.samuele._4h.GUIDb;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace lombardi.samuele._4h.GUIDb;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
